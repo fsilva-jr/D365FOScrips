e0bdd5a57318160d0e104acfbcb2380a614f75b8
fsilva-jr <50589817+fsilva-jr@users.noreply.github.com>
2025-04-23 08:00:27 -0300

    Add files via upload

---
+++ Invoke-D365FFOAxDBOptimization.ps1
@@ -0,0 +1,56 @@
+# https://github.com/valerymoskalenko/D365FFO-PowerShell-scripts/blob/master/Invoke-D365FFOAxDBOptimization.ps1
+#region Schedule script to Optimize Indexes on Databases -->
+$scriptPath = 'C:\Scripts'
+$scriptName = 'Optimize-AxDB.ps1'
+
+Write-Host "Installing Ola Hallengren's SQL Maintenance scripts"
+Import-Module -Name dbatools
+Install-DbaMaintenanceSolution -SqlInstance . -Database master -Solution IndexOptimize -ReplaceExisting
+Write-Host "Running Ola Hallengren's IndexOptimize tool"
+
+Write-Host "Saving Script..." -ForegroundColor Yellow
+$script = @'
+#region run Ola Hallengren's IndexOptimize
+    $sqlIndexOptimize = "EXECUTE master.dbo.IndexOptimize
+        @Databases = 'ALL_DATABASES',
+        @FragmentationLow = NULL,
+        @FragmentationMedium = 'INDEX_REORGANIZE,INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
+        @FragmentationHigh = 'INDEX_REBUILD_ONLINE,INDEX_REBUILD_OFFLINE',
+        @FragmentationLevel1 = 5,
+        @FragmentationLevel2 = 25,
+        @LogToTable = 'N',
+        @UpdateStatistics = 'ALL',
+        @OnlyModifiedStatistics = 'Y',
+        @MaxDOP = 0"
+
+         Import-Module -Name dbatools
+    Invoke-DbaQuery -SqlInstance localhost -Query $sqlIndexOptimize
+#endregion run Ola Hallengren's IndexOptimize
+'@
+
+$scriptFullPath = Join-Path $scriptPath $scriptName
+
+New-Item -Path $scriptPath -ItemType Directory -Force
+Set-Content -Value $script -Path $scriptFullPath -Force
+
+#region Apply SQL Connection settings <--
+Set-DbatoolsConfig -FullName sql.connection.trustcert -Value $true 
+Set-DbatoolsConfig -FullName sql.connection.encrypt -Value $false
+#endregion Apply SQL Connection settings -->
+
+Write-Host "Running Script for the first time... It may take about 30-60 minutes" -ForegroundColor Yellow
+Invoke-Expression $scriptFullPath
+
+Write-Host "Registering the Script as Scheduled Task to run it Daily..." -ForegroundColor Yellow
+$atStartUp =  New-JobTrigger -Daily -At "3:07 AM" -DaysInterval 1 -RandomDelay 00:40:00
+$option = New-ScheduledJobOption -StartIfIdle -MultipleInstancePolicy IgnoreNew
+Register-ScheduledJob -Name AXDBOptimizationDailyTask -Trigger $atStartUp -FilePath $scriptFullPath -ScheduledJobOption $option
+#Unregister-ScheduledJob -Name AXDBOptimizationDailyTask
+
+Write-Host "Registering the Script as Scheduled Task to run it at Startup..." -ForegroundColor Yellow
+$atStartUp = New-JobTrigger -AtStartup -RandomDelay 00:55:00
+$option = New-ScheduledJobOption -StartIfIdle -MultipleInstancePolicy IgnoreNew
+Register-ScheduledJob -Name AXDBOptimizationStartupTask -Trigger $atStartUp -FilePath $scriptFullPath -ScheduledJobOption $option
+#Unregister-ScheduledJob -Name AXDBOptimizationStartupTask
+
+#endregion Schedule script to Optimize Indexes on Databases <--
---
+++ Rotate-D365FFODevVMCertificates.ps1
@@ -0,0 +1,14 @@
+Write-Output "Rotating Certificates on OneBox VM"
+Set-Location -Path "cert:\LocalMachine\My"
+foreach($OldCert in Get-ChildItem -path Cert:\LocalMachine\My | Where {$_.NotAfter -lt $(get-date).AddMonths(2)})
+{
+    #$OldCert
+    $OldCert | Select Thumbprint, Subject, Notbefore, NotAfter | Format-Table
+    $NewCert = New-SelfSignedCertificate -CloneCert $OldCert -NotAfter (Get-Date).AddMonths(999)
+ 
+    (Get-Content 'C:\AOSService\webroot\web.config').Replace($OldCert.Thumbprint, $NewCert.Thumbprint) | Set-Content 'C:\AOSService\webroot\web.config'
+    (Get-Content 'C:\AOSService\webroot\wif.config').Replace($OldCert.Thumbprint, $NewCert.Thumbprint) | Set-Content 'C:\AOSService\webroot\wif.config'
+    (Get-Content 'C:\AOSService\webroot\wif.services.config').Replace($OldCert.Thumbprint, $NewCert.Thumbprint) | Set-Content 'C:\AOSService\webroot\wif.services.config'
+}
+Write-Output "IIS Reset..."
+iisreset 
